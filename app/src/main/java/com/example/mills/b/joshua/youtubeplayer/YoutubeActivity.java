04096e8de280705dd7a4ce3c6500bc990c63bfa6
Joshua Mills <joshua.mills@tcs.com>
2018-03-19 16:00:12 -0400

    youtube api fail setup

--- app/src/main/java/com/example/mills/b/joshua/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/example/mills/b/joshua/youtubeplayer/YoutubeActivity.java
@@ -1,18 +1,31 @@
 package com.example.mills.b.joshua.youtubeplayer;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
     implements YouTubePlayer.OnInitializedListener{
+    static final String  GOOGLE_API_KEY = "TODO: add api key";
+    static final String YOUTUBE_VIDEO_ID = "vrN4TOY8rSw";
+    static final String YOUTUBE_PLAYLIST = "PL7xvYrkzD7N_fpt9C1CWq6m_91YQmBd55";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout)getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+        YouTubePlayerView playerView = new YouTubePlayerView(this);
+        playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(playerView);
+        playerView.initialize(GOOGLE_API_KEY, this);
     }
 
     @Override
@@ -22,6 +35,13 @@ public class YoutubeActivity extends YouTubeBaseActivity
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
 
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this,REQUEST_CODE).show();
+        }else{
+            String errorMessage = String.format("There was an error initializing the YoutubePlayer (%1$s)", youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
